163cbc76870920aeb703e668b174846cf3e99159
Rdanzz <zidhanraffly123@gmail.com>
2025-03-30 18:53:41 +0700

    router

--- src/main.tsx
+++ src/main.tsx
@@ -2,37 +2,22 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import LoginPage from './pages/login.tsx'
+import { HashRouter, Routes, Route } from "react-router-dom";
 import RegisterPage from './pages/register.tsx'
 import ProductsPage from './pages/products.tsx'
 import ErrorPage from './pages/404.tsx'
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-  },
-  {
-    path: '/login',
-    element: <LoginPage />,
-  },
-  {
-    path: '/register',
-    element: <RegisterPage />,
-  },
-  {
-    path: '/products',
-    element: <ProductsPage />,
-  },
-  {
-    path: '*',
-    element: <ErrorPage />,
-  },
-])
-
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
-)
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/products" element={<ProductsPage />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </HashRouter>
+  </StrictMode>
+);
